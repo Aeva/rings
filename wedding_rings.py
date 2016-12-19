76919fd4283b57419193d7dfc6bcc91589f54184
Aeva Palecek <aeva@lunarfracking.com>
2016-12-18 18:03:12 -0600

    Initial commit.
    Put together a blend file to have reasonable defaults for generating
    rings, but ideally the only thing that will need to be checked in going
    forwards is just the python script.
    
    Python script generates two rings, one for me, and one for Mym.  Sizing
    is pulled out of a dictionary.  Still trying to find a good base size
    for both of us, then we can get fancy.

---
+++ wedding_rings.py
@@ -0,0 +1,56 @@
+import bpy
+
+# based on http://yzahk.in/3D-printable-ring-Blender-add-on
+
+
+SIZES = {
+    'Aeva' : 20.0,
+    'Mym'  : 22.0,
+}
+    
+
+
+def units(n):
+    #return n/1000
+    return n
+
+
+def ring_generator(wearer, thickness=5, segments=128):
+    diameter = SIZES[wearer]
+    
+    bpy.ops.mesh.primitive_circle_add(
+        vertices=segments, 
+        radius=units(diameter)/2,
+        enter_editmode=False, 
+    )
+    ob = bpy.context.active_object
+    ob.name = "{0}'s Ring".format(wearer)
+    me = ob.data
+    me.name = ob.name + 'Mesh'
+    
+    bpy.ops.object.mode_set( mode   = 'EDIT'   )
+    bpy.ops.mesh.select_mode( type  = 'VERT'   )
+    bpy.ops.mesh.select_all( action = 'SELECT' )
+
+    bpy.ops.mesh.extrude_region_move(
+        TRANSFORM_OT_translate={"value":(0, 0, units(thickness))}
+    )
+
+    bpy.ops.object.mode_set( mode = 'OBJECT' )
+    
+    bpy.ops.object.modifier_add(type = 'SOLIDIFY')
+    ob.modifiers['Solidify'].thickness = units(2)
+    
+    #bpy.ops.object.modifier_add(type = 'BEVEL')
+    #ob.modifiers['Bevel'].width = 1
+    #ob.modifiers['Bevel'].segments = 1
+
+    bpy.ops.object.modifier_add(type = "SUBSURF")
+    ob.modifiers['Subsurf'].levels = 2
+    ob.modifiers['Subsurf'].render_levels = 2
+    
+    return ob
+
+
+ring_generator('Aeva')
+ring_generator('Mym')
